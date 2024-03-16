6fda329836f955fb3b01ca2167ad00791e6c3dae
ehmasuk <ehmasuk@gmail.com>
2024-03-16 16:31:45 +0600

    john

--- index.php
+++ index.php
@@ -10,6 +10,8 @@
         <!-- google fonts -->
         <link href="https://fonts.googleapis.com/css2?family=Abel:wght@400;500;600&family=Inter:wght@400;500;600&display=swap" rel="stylesheet" />
         <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700;800&family=Roboto:wght@300;400;500;700;900&display=swap" rel="stylesheet" />
+        <!-- swal -->
+        <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 
         <link rel="stylesheet" href="style.css" />
         <title>Community</title>
@@ -66,7 +68,7 @@
                                 <p class="noti-title">Inbox</p>
                                 <div style="padding: 10px">
                                     <div class="single-notification">
-                                        <img src="https://i.pravatar.cc/150?4" alt="" />
+                                        <img src="./img/banner-media.png" alt="" />
                                         <div class="w-100">
                                             <p><b>Jose Lee fayez</b></p>
                                             <div class="d-flex align-items-center justify-content-between">
@@ -130,7 +132,7 @@
                         </div>
 
                         <div class="header-user-img">
-                            <a href="#"><img src="https://i.pravatar.cc/150?7" alt="" class="img-fluid" /></a>
+                            <a href="#"><img src="./img/banner-media.png" alt="" class="img-fluid" /></a>
                         </div>
                     </div>
                 </div>
@@ -141,6 +143,7 @@
             <div class="container">
                 <div class="row">
                     <div class="col-md-3 left-bar p-0">
+                        <div style="position:sticky;top:100px">
                         <div class="side-box">
                             <p class="bar-title">Top students</p>
                             <div class="side-box-post">
@@ -191,7 +194,7 @@
                         </div>
 
                         <div class="side-box">
-                            <p class="bar-title">Blogs</p>
+                            <p class="bar-title">Latest updates</p>
                             <div class="side-box-post">
                                 <div class="cover">
                                     <img src="https://i.pravatar.cc/150?11" alt="" />
@@ -210,45 +213,19 @@
                                     <p class="date">May 14, 2019</p>
                                 </div>
                             </div>
-                            <div class="side-box-post">
-                                <div class="cover">
-                                    <img src="https://i.pravatar.cc/150?13" alt="" />
-                                </div>
-                                <div class="info">
-                                    <b>Man is mortal</b>
-                                    <p class="date">May 14, 2019</p>
-                                </div>
-                            </div>
-                            <div class="side-box-post">
-                                <div class="cover">
-                                    <img src="https://i.pravatar.cc/150?14" alt="" />
-                                </div>
-                                <div class="info">
-                                    <b>Be honest</b>
-                                    <p class="date">May 14, 2019</p>
-                                </div>
-                            </div>
-                            <div class="side-box-post">
-                                <div class="cover">
-                                    <img src="https://i.pravatar.cc/150?15" alt="" />
-                                </div>
-                                <div class="info">
-                                    <b>Be honest</b>
-                                    <p class="date">May 14, 2019</p>
-                                </div>
-                            </div>
                             <button class="btn btn-sm blog-seemore-btn mt-4">SEE ALL</button>
+                        </div>
                         </div>
                     </div>
 
                     <div class="col-md-6 feed-bar">
-                        <h3 class="act-title">Activity Feed</h3>
+                        <h4 class="act-title">Activity Feed</h4>
 
                         <div class="post-box" onclick="toggleShow('post-popup-wraper')">
                             <div class="body">
                                 <div class="row align-items-center">
                                     <div class="user">
-                                        <img src="https://i.pravatar.cc/150?16" alt="" class="img-fluid" />
+                                        <img src="./img/banner-media.png" alt="" class="img-fluid" />
                                     </div>
                                     <div class="tap">
                                         <p>Share whats on your mind...</p>
@@ -257,13 +234,13 @@
                             </div>
                             <div class="footer">
                                 <div class="row">
-                                    <div class="icon" aria-label="Upload photo" data-microtip-position="top" role="tooltip"><i class="fas fa-camera"></i></div>
-                                    <div class="icon" aria-label="Upload video" data-microtip-position="top" role="tooltip"><i class="fas fa-video"></i></div>
+                                    <div class="icon"><i class="fas fa-camera"></i></div>
+                                    <div class="icon"><i class="fas fa-video"></i></div>
                                 </div>
                             </div>
                         </div>
 
-                        <?php
+<?php
 // Connect to MySQL
 $servername = "localhost";
 $username = "root";
@@ -292,7 +269,7 @@ if ($result->num_rows > 0) {
         <div class="feed-post" id="post_' . $row["id"] . '">
             <div class="post-header">
                 <div class="user">
-                    <img src="https://i.pravatar.cc/150?28" alt="" class="img-fluid" />
+                    <img src="./img/banner-media.png" alt="" class="img-fluid" />
                     <div>
                         <a href="#" class="name">John</a> posted an update
                         <p class="time">a year ago</p>
@@ -301,7 +278,7 @@ if ($result->num_rows > 0) {
                 <div class="icon delete-post" data-post-id="' . $row["id"] . '"><i class="fas fa-trash-alt"></i></div>
             </div>
             <div class="content">
-                <p style="font-size: 15px; margin-top: 10px">' . $row["text_content"] . '</p>';
+                <pre>' . $row["text_content"] . '</pre>';
         // Check if file_name is not NULL
         if ($row["file_name"] !== NULL) {
             echo '<img src="' . $row["file_name"] . '" alt="" />';
@@ -337,7 +314,7 @@ if ($result->num_rows > 0) {
         </div>';
     }
 } else {
-    echo "0 results";
+    echo '<div class="text-center p-5">No recent post avilable</div>';
 }
 
 // Close MySQL connection
@@ -346,11 +323,10 @@ $conn->close();
 
 
 
-
                     </div>
 
                     <div class="col-md-3 right-bar p-0">
-                        <div class="side-box">
+                        <div class="side-box" style="position:sticky;top:100px">
                             <p class="bar-title">Top books</p>
                             <div class="side-box-post">
                                 <div class="cover">
@@ -428,58 +404,18 @@ $conn->close();
                                 </div>
                             </div>
                         </div>
-                        <div class="side-box">
-                            <p class="bar-title">Latest updates</p>
-                            <div class="side-box-post">
-                                <div class="cover">
-                                    <img src="https://i.pravatar.cc/150?41" alt="" />
-                                </div>
-                                <div class="info">
-                                    <b>Be the best</b>
-                                    <p class="date">May 14, 2019</p>
-                                </div>
-                            </div>
-                            <div class="side-box-post">
-                                <div class="cover">
-                                    <img src="https://i.pravatar.cc/150?42" alt="" />
-                                </div>
-                                <div class="info">
-                                    <b>Be the best</b>
-                                    <p class="date">May 14, 2019</p>
-                                </div>
-                            </div>
-                            <div class="side-box-post">
-                                <div class="cover">
-                                    <img src="https://i.pravatar.cc/150?43" alt="" />
-                                </div>
-                                <div class="info">
-                                    <b>Tackle Your closest Spring cleaning</b>
-                                    <p class="date">May 14, 2019</p>
-                                </div>
-                            </div>
-                            <div class="side-box-post">
-                                <div class="cover">
-                                    <img src="https://i.pravatar.cc/150?44" alt="" />
-                                </div>
-                                <div class="info">
-                                    <b>Awesome tips and tricks</b>
-                                    <p class="date">May 14, 2019</p>
-                                </div>
-                            </div>
-                            <div class="side-box-post">
-                                <div class="cover">
-                                    <img src="https://i.pravatar.cc/150?45" alt="" />
-                                </div>
-                                <div class="info">
-                                    <b>I had a tree of money</b>
-                                    <p class="date">May 14, 2019</p>
-                                </div>
-                            </div>
-                        </div>
                     </div>
                 </div>
             </div>
         </section>
+
+
+
+
+
+        
+
+
 
         <div class="post-popup-wraper">
             <div class="post-popup">
@@ -488,38 +424,36 @@ $conn->close();
                     <i class="fa fa-times" onclick="toggleShow('post-popup-wraper')"></i>
                 </div>
                 <form id="postForm" enctype="multipart/form-data">
-    <div class="body">
-        <div class="top">
-            <img src="https://i.pravatar.cc/150?46" alt="" />
-            <p>John</p>
-        </div>
-        <div class="bottom">
-            <textarea id="text_content" name="text_content" placeholder="Share what's on your mind..."></textarea>
-        </div>
-        <div class="bottom">
-            <input type="hidden" name="MAX_FILE_SIZE" value="500000" />
-            <input type="file" id="file" name="file" accept="image/*" />
-        </div>
-    </div>
-    <div class="footer">
-        <div class="row">
-            <div class="icon"><i class="fas fa-camera"></i></div>
-            <div class="icon"><i class="fas fa-video"></i></div>
-        </div>
-        <button type="button" id="submitBtn" class="btn post-btn btn-sm">Post</button>
-    </div>
-</form>
+                    <div class="body">
+                        <div class="top">
+                            <img src="./img/banner-media.png" alt="" />
+                            <p>John</p>
+                        </div>
+                        <div class="bottom">
+                            <textarea required id="text_content" name="text_content" placeholder="Share what's on your mind..."></textarea>
+                        </div>
+                        <div class="bottom">
+                            <input type="hidden" name="MAX_FILE_SIZE" value="500000" />
+                        </div>
+                    </div>
+                    <div class="footer">
+                        <input type="file" id="file" name="file" accept="image/*" />
+                        <button type="button" id="submitBtn" class="btn post-btn btn-sm">Post</button>
+                    </div>
+                </form>
 
             </div>
         </div>
 
-
+<a href="">click me</a>
 
 
 
 <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 <script>
 $(document).ready(function(){
+
+
     // Delete post when trash icon is clicked
     $('.delete-post').click(function(){
         var postId = $(this).data('post-id');
@@ -530,22 +464,27 @@ $(document).ready(function(){
             success: function(response) {
                 // Remove the post from the DOM
                 $('#post_' + postId).remove();
-                alert('Post deleted successfully');
+                Swal.fire({
+                    title: "Deleted!",
+                    text: "Post successfully deleted!",
+                    icon: "success"
+                });
             },
             error: function(xhr, status, error) {
-                alert('Error deleting post');
+                Swal.fire({
+                    title: "Error!",
+                    text: "Something went wrong!",
+                    icon: "error"
+                });
             }
         });
     });
-});
 
 
 
 
 
-
-    $(document).ready(function() {
-        $('#submitBtn').click(function() {
+    $('#submitBtn').click(function() {
             var formData = new FormData($('#postForm')[0]);
 
             $.ajax({
@@ -555,16 +494,32 @@ $(document).ready(function(){
                 processData: false,
                 contentType: false,
                 success: function(response) {
-                    alert(response); // Show response from server
-                    // You can add more logic here to handle the response
-                    // For example, updating the UI or displaying a success message
+                    document.querySelector('.post-popup-wraper').classList.remove("show");
+
+                    Swal.fire({
+                        title: "Post uploaded!",
+                        text: "Post successfully uploaded!",
+                        icon: "success"
+                    });
+                    setTimeout(() => {
+                        window.location.reload(); 
+                    }, 2000);
                 },
                 error: function(xhr, status, error) {
                     alert('An error occurred: ' + error); // Show error message
                 }
             });
         });
-    });
+
+
+
+
+
+});
+
+
+
+document.querySelector('selector');
 
 
 
--- server.php
+++ server.php
@@ -1,5 +1,4 @@
 <?php
-// Connect to MySQL
 $servername = "localhost";
 $username = "root";
 $password = "";
@@ -7,56 +6,45 @@ $dbname = "community";
 
 $conn = new mysqli($servername, $username, $password, $dbname);
 
-// Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
-// Handle form submission
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $text_content = $_POST['text_content'];
 
-    // Initialize $target_file to NULL
     $target_file = NULL;
 
-    // Handle file upload if an image is selected
     if ($_FILES["file"]["size"] > 0) {
         $target_dir = "img/";
         $target_file = $target_dir . basename($_FILES["file"]["name"]);
         $uploadOk = 1;
         $imageFileType = strtolower(pathinfo($target_file, PATHINFO_EXTENSION));
 
-        // Check if file already exists
         if (file_exists($target_file)) {
             $uploadOk = 0;
         }
 
-        // Check file size
         if ($_FILES["file"]["size"] > 500000) {
             $uploadOk = 0;
         }
 
-        // Allow only certain file formats
         if ($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
             && $imageFileType != "gif") {
             $uploadOk = 0;
         }
 
-        // Check if $uploadOk is set to 0 by an error
         if ($uploadOk == 0) {
             echo "";
         } else {
-            // If everything is ok, try to upload file
             if (move_uploaded_file($_FILES["file"]["tmp_name"], $target_file)) {
                 echo "The file " . htmlspecialchars(basename($_FILES["file"]["name"])) . " has been uploaded.";
             }
         }
     }
 
-    // Insert data into the database
     $sql = "INSERT INTO posts (text_content, file_name) VALUES ('$text_content', ";
 
-    // Check if file_name should be NULL or a value
     if ($target_file) {
         $sql .= "'$target_file')";
     } else {
@@ -70,6 +58,5 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     }
 }
 
-// Close MySQL connection
 $conn->close();
 ?>
